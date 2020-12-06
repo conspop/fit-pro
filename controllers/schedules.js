7575f450d91378125376ac34e3763e2b2144a365
sebbby <seb.beitel@gmail.com>
2020-12-06 16:01:14 -0500

    build schedule array logic for getSchedule api call

--- controllers/schedules.js
+++ controllers/schedules.js
@@ -1,15 +1,78 @@
 const moment = require('moment')
 const dateHelpers = require('../expressutils/dateHelpers')
 
-const Contract = require('../models/contract');
-const Single = require('../models/single')
 const User = require('../models/user');
+const Single = require('../models/single')
 
 module.exports = {
   show
 };
 
 async function show(req, res) {
-  console.log(moment(req.query.startDate))
+  const timeframeStartDate = moment(req.query.startDate);
+  const timeframeEndDate = moment(req.query.endDate);
+  const id = req.user._id;
+
+  const schedule = [];
+  
+  // get array of singles within timeframe
+  const {singles} = await getSingles(timeframeStartDate, timeframeEndDate, id)
+  // get array of contracts whose start date is before the timeframe's end date
+  const {contracts} = await getContracts(timeframeStartDate, timeframeEndDate, id)
+
+  //push singles to schedule array
+  singles.forEach(single => schedule.push(single))
+
+  //loop through contracts and push instances of contract to schedule array
+  contracts.forEach(contract => {
+    console.log(contract)
+    console.log(contract._id)
+    const dateInstance = moment(contract.startDate)
+    while (dateInstance.isBefore(timeframeStartDate)) {
+      dateInstance.add(7, 'days')
+    }
+    while (dateInstance.isBefore(timeframeEndDate)) {
+      const {studio, style, time, classLength, rate, base, perHead, estimate, _id} = contract
+      const contractInstance = {
+        studio,
+        style,
+        time,
+        classLength,
+        rate,
+        base,
+        perHead,
+        estimate,
+        date: dateInstance,
+        contractId: _id
+      }
+      schedule.push(contractInstance)
+      dateInstance.add(7, 'days')
+    }
+  })
+
+  console.log(schedule)
+
+  res.json(schedule)
+
 }
+
+function getSingles(startDate, endDate, id) {
+  return User.
+    findById(id).
+    populate({
+      path: 'singles',
+      match: {date: {$gte: startDate, $lte: endDate}}
+    })
+}
+
+function getContracts(startDate, endDate, id) {
+  return User.
+    findById(id).
+    populate({
+      path: 'contracts',
+      match: {startDate: {$lte: endDate}}
+    })
+}
+
+
 
--- src/pages/SchedulePage/SchedulePage.jsx
+++ src/pages/SchedulePage/SchedulePage.jsx
@@ -3,22 +3,26 @@ import DatePicker from '../../components/DatePicker/DatePicker'
 import Stats from '../../components/Stats/Stats'
 import ClassList from '../../components/ClassList/ClassList'
 import apiService from '../../utils/apiService'
+import moment from 'moment'
 
 class  SchedulePage extends Component {
   state = {
-    startDate:'',
-    endDate:'',
+    startDate: moment('12-1-20'),
+    endDate: moment('12-31-20'),
+    schedule: ''
   }
 
-  handleDatesChange = (dates) => {
+  handleDatesChange = async (dates) => {
     this.setState({
       startDate: dates[0],
       endDate: dates[1],
       schedule: ''
     })
+    // const schedule = 
+    this.setState({schedule: await apiService.getSchedule(dates[0], dates[1])})
   }
 
-  componentDidUpdate = async () => {
+  componentDidMount = async () => {
     const schedule = await apiService.getSchedule(this.state.startDate, this.state.endDate)
     this.setState({schedule: schedule})
   }
